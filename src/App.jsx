a35f49b9880103f809ea5b09a277607a9e08a953
ShaunAhuja <ahuja.shaun00@gmail.com>
2025-02-17 19:14:14 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import Name from './components/Name.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Name/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Name.jsx
@@ -0,0 +1,16 @@
+function Name(){
+
+    return(
+        
+        <div className="nameBox">
+            <h1 className="name-title">Shaun Ahuja</h1>
+            <p className="name-description">Software Developer </p>
+            <p className="personal-description">
+            Software Engineer based in Toronto and New York, building passion projects and shipping meaningful code. </p>
+        </div>
+
+    );
+
+}
+
+export default Name
