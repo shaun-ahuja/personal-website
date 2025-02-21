75ec36c2e9f49599bf385c3097df70fb98ccd976
ShaunAhuja <ahuja.shaun00@gmail.com>
2025-02-21 14:54:01 -0500

    adding about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // import { useState } from 'react'
 import Name from './components/Name.jsx'
 import Navbar from './components/Navbar.jsx'
+import About from './components/About.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
-      <Name/>
+      <Navbar />
+      <Name />
+      <About />
+
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+    return (
+        <div className="about-box">
+            <h2 className="about-header">About Me</h2>
+
+            <p className="description-one">Currently a Computer Science student at the University of Western Ontario.</p>
+
+            <p className="description-two">If I'm not building something cool or finishing a last minute assignment, you can find me at the golf course, going out with friends, or learning about cars. </p>
+
+            <p className="description-three">I really like music, make sure you scroll to the bottom to see what I'm listening to these days :&#41; </p>
+
+        </div>
+
+
+    );
+
+}
+
+export default About
