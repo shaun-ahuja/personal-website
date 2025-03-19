9ce38ab4464324b08c37441bee0312e46a9b5897
ShaunAhuja <sahuja47@uwo.ca>
2025-03-18 23:32:19 -0400

    trying to fix about section on iphone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,3 @@
-// import "./Navbar.css"
-
-// function Navbar() {
-
-//     return (
-//         <div className="navbar">
-//             <button className="name-logo">SHAUN AHUJA</button>
-//             <button onClick="document.getElementById('about-container').scrollIntoView({ behavior:'smooth'});" className="about-button">About</button>
-//             <button className="experience-button">Experience</button>
-//             <button className="projects-button">Projects</button>
-//         </div >
-//     );
-// }
-// export default Navbar
-
-
 import React from 'react';
 import "./Navbar.css";
 
